fd8d2161cee6a6e4ee999d788741a590fd276fea
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2022-10-19 23:01:51 +0530

    Added solution - LeetHub

---
+++ Max sum in the configuration - GFG/max-sum-in-the-configuration.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Maxsum_Among_All_Rotations_Array
+{
+	public  static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			
+			System.out.println(new GfG().max_sum(arr,n));
+			
+		t--;
+		}
+	}
+	
+}
+
+// } Driver Code Ends
+
+
+class GfG
+{
+    int max_sum(int arr[], int n)
+    {
+        // Your code here
+        int sum = 0;
+        int s0 = 0;
+        
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            s0 += (arr[i] * i);
+        }
+        
+        int pre = s0;
+        int ans = s0;
+        
+        for(int i=0;i<n-1;i++){
+            int sn = pre + sum - (n*arr[n-i-1]);
+            if(sn > ans){
+                ans = sn;
+            }
+            pre = sn;
+        }
+        return ans;
+    }	
+}
