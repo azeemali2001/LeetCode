33109150f0ca04ee12132f55c78de933656496c0
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2022-10-24 12:52:39 +0530

    Added solution - LeetHub

---
+++ Count the number of possible triangles - GFG/count-the-number-of-possible-triangles.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        while(t > 0)
+        {
+            //taking elements count
+            int n = sc.nextInt();
+            
+            //creating an array of length n
+            int arr[] = new int[n];
+            
+            //adding elements to the array
+            for(int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+            
+            //creating an object of class Solutions
+            Solution ob = new Solution();
+            
+            //calling the method findNumberOfTriangles()
+            //of class Solutions and printing the results
+            System.out.println(ob.findNumberOfTriangles(arr,n));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to count the number of possible triangles.
+    static int findNumberOfTriangles(int arr[], int n)
+    {
+        // code here
+        Arrays.sort(arr);
+        
+        int count = 0;
+        
+        for(int i=arr.length-1;i>1;i--){
+            int lo = 0;
+            int hi = i-1;
+            
+            while(lo < hi){
+                if(arr[lo] + arr[hi] > arr[i]){
+                    count += (hi-lo);
+                    hi--;
+                } else {
+                    lo++;
+                }
+            }
+        }
+        return count;
+    }
+}
