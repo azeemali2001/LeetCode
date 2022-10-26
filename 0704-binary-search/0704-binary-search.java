3b7d9bce15ef11dc96154e47531a4f36dca0ecf3
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2022-10-26 18:39:36 +0530

    Time: 0 ms (100.00%), Space: 54.1 MB (63.89%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int t) {
+        int lo = 0;
+        int hi = nums.length-1;
+        
+        while(lo <= hi)
+        {
+            int mid = (lo+hi)/2;
+            if(nums[mid] == t){
+                return mid;
+            }else if(nums[mid] < t){
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+        return -1;
+    }
+}
