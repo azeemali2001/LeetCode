cadaecbd297518526e608f5e5aca6b95c78a3656
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2022-12-01 01:44:44 +0530

    Added solution - LeetHub

---
+++ Cyclically rotate an array by one - GFG/cyclically-rotate-an-array-by-one.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+        int pre = arr[0];
+        int pre2 = arr[0];
+        boolean flag = true;
+        arr[0] = arr[n-1];
+        for(int i=1;i<n;i++) {
+            if(flag) {
+                pre2 = arr[i];
+                arr[i] = pre;
+            } else {
+                pre = arr[i];
+                arr[i] = pre2;
+            }
+            flag = !flag;
+        }
+        
+        
+        
+    }
+}
