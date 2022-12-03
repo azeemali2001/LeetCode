1d2c59caa0273dd41197fe96aaf0a2c14fac04f7
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2022-12-03 16:22:17 +0530

    Time: 13 ms (78.71%), Space: 55.2 MB (48.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,8 +1,10 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        //Sort Using Lambda Function :-
         Arrays.sort(intervals,(a,b)->{
             return a[0]-b[0];
         });
+        
         
         ArrayList<int[]> list = new ArrayList<>();
         list.add(intervals[0]);
@@ -13,17 +15,16 @@ class Solution {
             
             int sp2 = intervals[i][0];
             int ep2 = intervals[i][1];
+
             
             if(ep1 >= sp2){
                 // Merging
                 list.get(list.size()-1)[1] = Math.max(ep1,ep2);
             } else {
                 //No merging
-                int[] tmp = new int[2];
-                tmp[0] = sp2;
-                tmp[1] = ep2;
-                list.add(tmp);
+                list.add(intervals[i]);
             }
+            
         }
         
         int[][] ans = list.toArray(new int[list.size()][2]);
