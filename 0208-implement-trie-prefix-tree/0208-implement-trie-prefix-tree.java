c46f9aec32ba7f470e4173ab41241da8d0b465ac
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2023-03-19 14:02:22 +0530

    Time: 38 ms (68.57%), Space: 51.6 MB (62.57%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,101 +1,60 @@
-class Node{
-    char ch;
-    int count;
-    boolean isEnd;
-    HashMap<Character,Node> children;
-    
-    Node(char ch){
-        this.ch = ch;
-        isEnd = false;
-        children = new HashMap<>();
-    }
-    
-    boolean hasChild(char ch){
-        return this.children.containsKey(ch);
-    }
-    
-    Node getNode(char ch){
-        return this.children.get(ch);
-    }
-    
-    Node createNode(char ch){
-        Node node = new Node(ch);
-        this.children.put(ch,node);
-        return node;
-    }
-    
-    void markTrue(){
-        this.isEnd = true;
-    }
-    
-    boolean isEndOfWord(){
-        return this.isEnd;
-    }
-    
-    int changeCount(int v){
-        return this.count = v;
-    }
-    
-    int getCount(){
-        return this.count;
-    }
-}
-
 class Trie {
     
-    Node root;
-
+    class Node {
+        Node[] children;
+        boolean isEnd;
+        
+        Node() {
+            children = new Node[26];
+            isEnd = false;
+        }
+    }
+    
+    final private Node root;
+    
     public Trie() {
-        root = new Node('-');
+        root = new Node();
     }
     
     public void insert(String word) {
-        Node ptr = root;
+        Node curr = root;
         
-        
-        
-        for(int i=0;i<word.length();i++){
-            
-            
+        for(int i=0;i<word.length();i++) {
             char ch = word.charAt(i);
-            if(ptr.hasChild(ch)){
-                ptr = ptr.getNode(ch);
-            } else {
-                ptr = ptr.createNode(ch);
-            }
-            ptr.changeCount(ptr.getCount()+1);
             
+            if(curr.children[ch-'a'] == null) {
+                curr.children[ch-'a'] = new Node();
+            }
+            curr = curr.children[ch-'a'];
         }
-        ptr.markTrue();
+        curr.isEnd = true;
+        
     }
     
     public boolean search(String word) {
-        Node ptr = root;
-        for(int i=0;i<word.length();i++){
+        Node curr = root;
+        
+        for(int i=0;i<word.length();i++) {
             char ch = word.charAt(i);
-            if(ptr.hasChild(ch)){
-                ptr = ptr.getNode(ch);
-            } else {
-                return false;
-            }
+            
+            if(curr.children[ch-'a'] == null) return false;
+            curr = curr.children[ch-'a'];
         }
         
-        return ptr.isEndOfWord();
+        return curr.isEnd;
     }
     
     public boolean startsWith(String prefix) {
+        Node curr = root;
         
-        Node ptr = root;
-        
-        for(int i=0;i<prefix.length();i++){
+        for(int i=0;i<prefix.length();i++) {
             char ch = prefix.charAt(i);
-            if(ptr.hasChild(ch)){
-                ptr = ptr.getNode(ch);
-            } else {
-                return false;
-            }
+            
+            if(curr.children[ch-'a'] == null) return false;
+            curr = curr.children[ch-'a'];
         }
-        return ptr.getCount() > 0;
+        
+        return true;
     }
 }
 
