ccbf008f05cdeab2c51405af1dfe17134b826680
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2023-04-02 06:21:35 +0530

    Time: 0 ms (100.00%), Space: 39 MB (73.27%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,24 @@
 class Solution {
     public int fib(int n) {
+        return fib_(n, new int[n+1]);
+    }
+    
+    public int fib_(int n, int[] qb) {
         if(n == 0 || n == 1) {
             return n;
         }
         
-        int fibnm1 = fib(n-1);
-        int fibnm2 = fib(n-2);
+        if(qb[n] != 0) {
+            return qb[n];
+        }
+        
+        int fibnm1 = fib_(n-1, qb);
+        int fibnm2 = fib_(n-2, qb);
         int fibn = fibnm1 + fibnm2;
+        
+        qb[n] = fibn;
         
         return fibn;
     }
+    
 }
