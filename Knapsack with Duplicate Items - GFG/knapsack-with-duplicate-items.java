e96ab0a27275644845fa9e29ddb319384985623b
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2023-04-02 14:41:46 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        int[] dp = new int[W+1];
+        
+        for(int i=0;i<dp.length;i++) {
+            int max = 0;
+            for(int j=0;j<N;j++) {
+                if(i>=wt[j]) {
+                    int rbagv =dp[i-wt[j]];
+                    int tbagv = val[j] + rbagv;
+                    
+                    if(tbagv > max) {
+                        max = tbagv;
+                    }
+                }
+            }
+            
+            dp[i] = max;
+        }
+        
+        return dp[W];
+    }
+}
