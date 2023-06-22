518e66d34e1e2a61455f114e8d90875515ac7126
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2023-06-22 15:53:32 +0530

    Time: 5 ms (70.24%), Space: 54.3 MB (65.07%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,34 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
+        int bs = -1 * prices[0];
+        int ss = 0;
         
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = -prices[0];
-        dp[0][1] = 0;
-            
-            
-        for(int i=1;i<prices.length;i++){
-            //Set Buying Price
-            int b1 = dp[i-1][1]-prices[i];
-            int b2 = dp[i-1][0];
-            
-            if(b1 > b2){
-                dp[i][0] = b1;
-            } else {
-                dp[i][0] = b2;
+        for(int i=1;i<prices.length;i++) {
+            // Buying State
+            int temp = Math.max(bs, ss - prices[i]);
+            if(temp > bs) {
+                bs = temp;
             }
             
             
-            //Set Selling Price
-            int s1 = prices[i]+dp[i-1][0]-fee;
-            int s2 = dp[i-1][1];
-            
-            if(s1 > s2){
-                dp[i][1] = s1;
-            } else {
-                dp[i][1] = s2;
+            // Selling State
+            temp = prices[i] + bs - fee;
+            if(temp > ss) {
+                ss = temp;
             }
         }
         
-        return dp[prices.length-1][1];
+        return ss;
     }
 }
