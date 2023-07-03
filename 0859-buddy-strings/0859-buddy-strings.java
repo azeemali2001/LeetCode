df9ca52dabdd6e261bb469dc44aa6a3056a34108
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2023-07-03 18:53:36 +0530

    Time: 5 ms (28.75%), Space: 41 MB (99.29%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -2,52 +2,27 @@ class Solution {
     public boolean buddyStrings(String s, String goal) {
         if(s.length() != goal.length()) return false;
         
-        HashMap<Character, Integer> hm = new HashMap<>();
-        
-        int idx1 = 0;
-        int idx2 = 1;
-        
-        boolean flag = false;
-        
-        for(int i=0;i<s.length();i++) {
-            
-            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i),0)+1);
-            if(s.charAt(i) != goal.charAt(i)) {
-                if(flag) {
-                    idx2 = i;
-                    break;
-                } else {
-                    idx1 = i;
-                    flag = true;
-                }
-            }
-        }
-        
-        if(flag == false)  {
-            if(s.length() < 2) return false;
-            
-            for(Character key : hm.keySet()) {
-                if(hm.get(key) > 1) {
-                    return true;
-                } 
-            }
-            
-            return false;
-            
-        }
-        
-        char[] arr = s.toCharArray();
-        
-        char temp = arr[idx1];
-        arr[idx1] = arr[idx2];
-        arr[idx2] = temp;
-        
-        s = new String(arr);
-        
         if(s.equals(goal)) {
-                return true;
-            } else {
-                return false;
+            Set<Character> hs = new HashSet<>();
+            
+            for(Character a : s.toCharArray()) {
+                hs.add(a);
             }
+            
+            return hs.size() < s.length();
+        }
+        
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i) != goal.charAt(i)) {
+                list.add(i);
+            }
+        }
+        
+        
+        if(list.size() == 2 && s.charAt(list.get(0)) == goal.charAt(list.get(1)) &&                          s.charAt(list.get(1)) == goal.charAt(list.get(0))) return true;
+        
+        return false;
     }
 }
