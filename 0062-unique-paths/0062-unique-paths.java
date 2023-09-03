56b329ba5d3258e0466e6b77bedfd6596ceed29e
Azeem Ali <90110263+azeemali2001@users.noreply.github.com>
2023-09-03 12:57:17 +0530

    Time: 0 ms (100.00%), Space: 39 MB (78.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,19 @@
 class Solution {
+    int[][] dp;
     public int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1) return 1;
-        
-        m--;
-        n--;
-        
-        if(m < n) {
-            m = m+n;
-            n = m-n;
-            m = m-n;
-        }
-        
-        long res = 1;
-        int j = 1;
-        
-        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
-            res *= i;
-            res /= j;
-        }
-            
-        return (int)res;
+        dp = new int[m][n];
+        return dfs(0,0,m,n);
     }
+    
+    public int dfs(int i, int j, int m, int n)
+    {
+        if(i == m-1 && j == n-1) return 1;
+        
+        if(i >= m || j >= n) return 0;
+        
+        if(dp[i][j] != 0) return dp[i][j];
+        
+        return dp[i][j] = dfs(i+1, j, m, n) + dfs(i, j+1, m, n);
+    }
+    
 }
